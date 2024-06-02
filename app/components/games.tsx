79d6b5bc8100fc7a0921e506c3c5f4c241c2854b
Gniewko Koscielak <gniewkoscielak@gmail.com>
2024-06-02 21:09:47 +0200

    Refactor game card layout and styling

--- app/components/games.tsx
+++ app/components/games.tsx
@@ -6,26 +6,32 @@ export default async function Games() {
   const games = await getAllGames();
   console.log("GAMES FROM PAGE", games);
   return (
-    <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 ">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
       {games?.map((game) => {
         return (
-          <div className=" bg-stone-500 rounded-2xl p-4 m-3" key={game.id}>
-            <h2>{game.title}</h2>
-            <p>Studio: {game.studio}</p>
+          <div className="bg-stone-500 rounded-2xl p-4 m-3" key={game.id}>
+            <h2 className="text-xl font-bold mb-2">{game.title}</h2>
+            <p className="font-semibold">
+              Studio: <span className="font-normal">{game.studio}</span>
+            </p>
 
             <img
-              className=" w-max object-cover rounded-lg shado"
+              className="w-full object-cover rounded-lg shadow-lg mb-4"
               src={game.image}
-              alt=""
+              alt={game.title}
             />
-            <p>
+            <p className="mb-4">
               {game.description} <br />
             </p>
-            <a target="_blank" href={game.www}>
+            <a
+              target="_blank"
+              className="text-blue-200 underline mb-4 block"
+              href={game.www}
+            >
               {game.title + " Website"}
             </a>
-            <p>
-              Release date:{" "}
+            <p className="mb-4">
+              <span className="font-semibold">Release date:</span>{" "}
               {new Date(game.release_date).toLocaleDateString("pl-PL", {
                 day: "2-digit",
                 month: "2-digit",
@@ -33,9 +39,11 @@ export default async function Games() {
               })}
             </p>
 
-            <p>Game type: {game.game_type}</p>
+            <p className="mb-4">
+              <span className="font-semibold">Game type:</span> {game.game_type}
+            </p>
             <RatingAvg gameId={game.id as number} />
-            <hr />
+            <hr className="my-4" />
             <RatingForm gameId={game.id as number} />
           </div>
         );
