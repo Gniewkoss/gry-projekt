e3aa928f42b489850b9e4ec463f198478610cb03
Gniewko Koscielak <gniewkoscielak@gmail.com>
2024-06-03 12:07:52 +0200

    Update navbar layout and styles

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -6,14 +6,16 @@ export default function Navbar() {
   const router = useRouter();
 
   return (
-    <div className="flex flex-row bg-stone-700 w-screen p-8 justify-between">
+    <div className="flex flex-col sm:flex-row items-center bg-stone-700 w-full p-8 justify-between">
       <button onClick={() => router.push("/")}>
-        <h1 className="text-2xl">GamesApp</h1>
+        <h1 className="text-2xl sm:text-xl text-white mr-0 sm:mr-10">
+          GamesApp
+        </h1>
       </button>
-      <div className="flex gap-4">
+      <div className="flex gap-4 flex-col sm:flex-row items-center mt-4 sm:mt-0">
         <button
           onClick={() => router.push("/company/add-new")}
-          className="bg-red-500 rounded-lg px-3 py-1"
+          className="bg-red-500 text-white rounded-lg px-3 py-2 sm:px-4 sm:py-2 sm:h-10"
         >
           Register your company
         </button>
