cb300ae0cea11c9215341ce977df05d172d67829
Gniewko Koscielak <gniewkoscielak@gmail.com>
2024-06-03 12:53:49 +0200

    Update styling and layout in AddNewGame component

--- app/game/add-new/page.tsx
+++ app/game/add-new/page.tsx
@@ -15,36 +15,52 @@ export default function AddNewGame() {
   return (
     <div>
       <div className="flex flex-col items-center justify-center h-screen ">
-        <h1 className="m-5 ">Add new Game to your commpany</h1>
+        <h1 className="m-5 mt-40 ">Add new Game to your commpany</h1>
 
         <form
           action={addGame}
-          className="flex flex-col gap-4 bg-gray-200 p-10 rounded-2xl w-1/2"
+          className="flex flex-col gap-4 bg-gray-200 p-6 sm:p-10 rounded-2xl w-full sm:w-2/3 md:w-1/2 lg:w-1/3"
         >
-          <label>
+          <label className="flex flex-col">
             Title
-            <input name="title" type="text" required />
+            <input
+              name="title"
+              type="text"
+              required
+              className="rounded-lg p-2 mt-1"
+            />
           </label>
-          <label>
+          <label className="flex flex-col">
             Description
-            <input name="description" type="text" />
+            <input
+              name="description"
+              type="text"
+              className="rounded-lg p-2 mt-1"
+            />
           </label>
-          <label>
+          <label className="flex flex-col">
             Website
-            <input name="www" type="text" />
+            <input name="www" type="text" className="rounded-lg p-2 mt-1" />
           </label>
-
-          <label>
+          <label className="flex flex-col">
             Image
-            <input name="image" type="text" />
+            <input name="image" type="text" className="rounded-lg p-2 mt-1" />
           </label>
-          <label>
+          <label className="flex flex-col">
             Type
-            <input name="game_type" type="text" />
+            <input
+              name="game_type"
+              type="text"
+              className="rounded-lg p-2 mt-1"
+            />
           </label>
-          <label>
+          <label className="flex flex-col">
             Release date
-            <input name="release_date" type="date" />
+            <input
+              name="release_date"
+              type="date"
+              className="rounded-lg p-2 mt-1"
+            />
           </label>
           <input
             name="company_id"
@@ -53,7 +69,9 @@ export default function AddNewGame() {
             required
             hidden
           />
-          <button className="bg-green-500 rounded-lg p-2">Submit</button>
+          <button className="bg-green-500 text-white rounded-lg p-2 mt-4">
+            Submit
+          </button>
         </form>
       </div>
     </div>
